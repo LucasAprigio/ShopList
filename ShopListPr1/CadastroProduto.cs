9b496d25d378bfbe12cd6b369767a14ca4bca63b
LucasAprigio <93295707+LucasAprigio@users.noreply.github.com>
2022-07-13 10:52:35 -0300

    .

--- ShopListPr1/CadastroProduto.cs
+++ ShopListPr1/CadastroProduto.cs
@@ -164,7 +164,7 @@ namespace ShopListPr1
 
         private void txtQuantidade_TextChanged_1(object sender, EventArgs e)
         {
-           double total = Convert.ToDouble(txtPreco.Text) * Convert.ToDouble(txtQuantidade.Text);
+           double total = Convert.ToDouble(txtPreco.Text) * Convert.ToInt32(txtQuantidade.Text);
            txtTotal.Text = total.ToString();
 
         }
